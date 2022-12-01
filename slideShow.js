fcc967439a3d16f95205e4c9cd45d6ac5e5b6908
AndrewMurphy <amurf26@outlook.com>
2022-11-30 18:12:46 -0700

    Changed slideshow for less whitespace

--- slideShow.js
+++ slideShow.js
@@ -13,19 +13,37 @@ function currentSlide(n) {
 
 // moves slides after button click
 function showSlides(n) {
-  let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
+  let slideShow = document.getElementById("slideShow");
+
+  
+  let i;
   if (n > slides.length) {slideIndex = 1}
+
+  
   if (n < 1) {slideIndex = slides.length}
+  
+  
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+  
+  
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  
+  if(slideIndex == slides.length){
+    slideShow.append(slides[slideIndex-1], slides[slideIndex-slides.length]);
+    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-slides.length].style.display = "block";
+  }else if(slideIndex != slides.length){
+    slideShow.append(slides[slideIndex-1], slides[slideIndex]);
+    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex].style.display = "block";
+  }
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
